576431dad614a5ed62dff8d24315972953b8b54b
Miguel <miguelaf71@hotmail.com>
2023-05-22 21:55:51 +0200

    formulario evaluacion sin fecha

--- js/evaluacion/detalle.js
+++ js/evaluacion/detalle.js
@@ -1,35 +1,37 @@
-const textarea = document.querySelector('textarea');
-const acceptButton = document.getElementById('acceptButton');
+const textarea = document.querySelector("textarea");
+const acceptButton = document.getElementById("acceptButton");
 const evaluacionValor = document.getElementById("evaluacion-valor");
 
-if(localStorage.getItem("rol")=="no_admin" || localStorage.getItem("rol") == "admin"){
-  textarea.addEventListener('focus', function() {
+if (
+  localStorage.getItem("rol") == "no_admin" ||
+  localStorage.getItem("rol") == "admin"
+) {
+  textarea.addEventListener("focus", function () {
     adjustHeight(this);
-    acceptButton.className +=" back-button"
-    acceptButton.style.display='flex';
+    acceptButton.className += " back-button";
+    acceptButton.style.display = "flex";
   });
 }
 
-if(localStorage.getItem("rol")=="no_admin"){
-  configureStars()
+if (localStorage.getItem("rol") == "no_admin") {
+  configureStars();
 }
 
 function adjustHeight(textarea) {
-  textarea.style.border = '1px solid #ccc';
-  textarea.style.borderLeft = 'none';
-  textarea.style.height = 'auto';
-  textarea.style.height = textarea.scrollHeight + 'px';
+  textarea.style.border = "1px solid #ccc";
+  textarea.style.borderLeft = "none";
+  textarea.style.height = "auto";
+  textarea.style.height = textarea.scrollHeight + "px";
 }
-
 
 if (localStorage.getItem("token")) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  
+
   const id = urlParams.get("id");
   const obraActual = urlParams.get("obraActual");
   const idOp = urlParams.get("idOp");
-  
+
   console.log(id);
   const feedbackCliente = urlParams.get("cliente");
   const feedbackObra = urlParams.get("obra");
@@ -42,27 +44,33 @@ if (localStorage.getItem("token")) {
 
   const nombreObra = document.getElementById("evaluacion-obra");
   nombreObra.innerText = feedbackObra;
-  
-  const evaluacionDescripcion = document.getElementById("evaluacion-descripcion");
-  
+
+  const evaluacionDescripcion = document.getElementById(
+    "evaluacion-descripcion"
+  );
 
   const botonAtrasFeedback = document.getElementById("feedbackAtras");
-  botonAtrasFeedback.setAttribute("href","./empleado-detalle.html?id="+idOp+"&obraActual="+obraActual)
-
+  botonAtrasFeedback.setAttribute(
+    "href",
+    "./empleado-detalle.html?id=" + idOp + "&obraActual=" + obraActual
+  );
 
   fetch("http://localhost:8000/evaluaciones/id/" + id).then((res) =>
-  res.json().then((res) => {
-    console.log(res);
-    if(!res){
-    feedback = res;
-    evaluacionDescripcion.innerText = feedback.descripcion;
-    evaluacionValor.innerText = feedback.evaluacion;
-    }else{
-      feedback = null;
-      evaluacionDescripcion.innerText = " ";
-      evaluacionValor.innerText = " "; 
-    }
-  }))
+    res.json().then((res) => {
+      
+      if (!res) {
+        feedback = null;
+        evaluacionDescripcion.innerText = " ";
+        evaluacionValor.innerText = 0;
+        
+      } else {
+        
+        feedback = res;
+        evaluacionDescripcion.innerText = feedback.descripcion;
+        evaluacionValor.innerText = feedback.evaluacion;
+      }
+    })
+  );
 }
 function configureStars() {
   const stars = document.getElementsByClassName("interactive-star");
@@ -70,53 +78,49 @@ function configureStars() {
   for (let i = 0; i < stars.length; i++) {
     stars[i].addEventListener("click", () => {
       if (stars[i].getAttribute("class").includes("staroff")) {
-        for (let j=0; j<=i; j++) {
+        for (let j = 0; j <= i; j++) {
           turnYellow(stars[j]);
         }
-        evaluacionValor.innerText = i+1;
+        evaluacionValor.innerText = i + 1;
       } else {
-        for (let j = stars.length-1; j > i; j--) {
+        for (let j = stars.length - 1; j > i; j--) {
           turnGray(stars[j]);
         }
-        evaluacionValor.innerText = i+1;
+        evaluacionValor.innerText = i + 1;
       }
     });
   }
   // const dato = document.getElementById("evaluacion-valor");
   // console.log(dato);
-  
 }
 function turnYellow(star) {
   star.classList.remove("staroff");
   star.classList.add("star");
-  
 }
 
 function turnGray(star) {
   star.classList.remove("star");
   star.classList.add("staroff");
-  
 }
 
-acceptButton.addEventListener("click", async function(){
-  
+acceptButton.addEventListener("click", async function () {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
 
   let obra = "";
   let operario = "";
 
-  if (localStorage.getItem("rol") == "no-admin"){
+  if (localStorage.getItem("rol") == "no_admin") {
     obra = urlParams.get("obraActual");
+    // console.log("id obra---->", obra);
     operario = urlParams.get("id");
-  }else{
+  } else {
     const miSelectorO = document.getElementsByClassName("selectorO");
     obra = miSelectorO.value;
-    operario = urlParams.get("idOp")
+    operario = urlParams.get("idOp");
   }
-  
 
-  console.log("accept button clicked")
+  console.log("accept button clicked");
   const res = await fetch("http://localhost:8000/evaluaciones", {
     method: "POST",
     headers: {
@@ -124,22 +128,23 @@ acceptButton.addEventListener("click", async function(){
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      id_operario:operario,
-      evaluacion:parseInt(evaluacionValor.textContent, 10),
+      id_operario: operario,
+      evaluacion: parseInt(evaluacionValor.textContent, 10),
       descripcion: document.getElementById("evaluacion-descripcion").value,
-      id_supervisor: localStorage.getItem("id"),
-      id_obra: obra
+      id_usuario: localStorage.getItem("id"),
+      id_obra: obra,
     }),
   });
 
   const response = await res.json();
-  console.log(response)
+  console.log(response);
   location.reload();
 });
 
-
-if (localStorage.getItem("rol") == "admin" || localStorage.getItem("rol") == "dev") {
-
+if (
+  localStorage.getItem("rol") == "admin" ||
+  localStorage.getItem("rol") == "dev"
+) {
   const pEmpleadoMainClave = document.getElementById("clave-cliente");
   pEmpleadoMainClave.classList.add("oculto");
   const pEmpleadoMainValor = document.getElementById("evaluacion-cliente");
@@ -153,7 +158,6 @@ if (localStorage.getItem("rol") == "admin" || localStorage.getItem("rol") == "de
   selectorClientes.setAttribute("class", "selectorC");
   labelClientes.appendChild(selectorClientes);
 
-
   const labelObras = document.createElement("label");
   labelObras.textContent = "Obra: ";
   const selectorObras = document.createElement("select");
@@ -161,43 +165,42 @@ if (localStorage.getItem("rol") == "admin" || localStorage.getItem("rol") == "de
   labelObras.appendChild(selectorObras);
 
   divDetalleMain.appendChild(labelClientes);
-  //Rellenamos los datos de los clientes  
+  //Rellenamos los datos de los clientes
   //Creamos el primer elemento de los selectores con texto vacio
   const opcionVaciaC = document.createElement("option");
   opcionVaciaC.value = -1;
-  opcionVaciaC.textContent = " "
+  opcionVaciaC.textContent = " ";
   selectorClientes.appendChild(opcionVaciaC);
   fetch("http://localhost:8000/clientes/")
-  .then(res => res.json())
-  .then(clientes => {
-    clientes.forEach(cliente => {
-      const  opcionC = document.createElement("option");
-      opcionC.value = cliente._id;
-      opcionC.textContent = cliente.nombre;
-      selectorClientes.appendChild(opcionC);
-    });
-  })
-  .catch(error => console.log(error));
-  
+    .then((res) => res.json())
+    .then((clientes) => {
+      clientes.forEach((cliente) => {
+        const opcionC = document.createElement("option");
+        opcionC.value = cliente._id;
+        opcionC.textContent = cliente.nombre;
+        selectorClientes.appendChild(opcionC);
+      });
+    })
+    .catch((error) => console.log(error));
+
   divDetalleMain.appendChild(labelObras);
   //Rellenamos los datos de las obras
   const opcionVaciaO = document.createElement("option");
   opcionVaciaO.value = -1;
-  opcionVaciaO.textContent = " "
+  opcionVaciaO.textContent = " ";
   selectorObras.appendChild(opcionVaciaO);
   fetch("http://localhost:8000/obras/")
-  .then(res => res.json())
-  .then(obras => {
-    obras.forEach(obra => {
-      const  opcionO = document.createElement("option");
-      opcionO.value = obra._id;
-      opcionO.textContent = obra.nombre;
-      selectorObras.appendChild(opcionO);
-    });
-  })
-  .catch(error => console.log(error));
-
-}else{
+    .then((res) => res.json())
+    .then((obras) => {
+      obras.forEach((obra) => {
+        const opcionO = document.createElement("option");
+        opcionO.value = obra._id;
+        opcionO.textContent = obra.nombre;
+        selectorObras.appendChild(opcionO);
+      });
+    })
+    .catch((error) => console.log(error));
+} else {
   const pEmpleadoMainClave = document.getElementById("clave-cliente");
   pEmpleadoMainClave.classList.remove("oculto");
   const pEmpleadoMainValor = document.getElementById("evaluacion-cliente");
