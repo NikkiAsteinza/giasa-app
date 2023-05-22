211d24fbd41e290f716d4e6bb98a4400c4f65498
Miguel <miguelaf71@hotmail.com>
2023-05-23 01:07:20 +0200

    estilos error login

--- js/auth/login.js
+++ js/auth/login.js
@@ -1,8 +1,9 @@
 const mainContainer = document.getElementById("main-container");
 if (!localStorage.getItem("token")) {
   mainContainer.innerHTML =
-    '<form id="login-form"><div><input id="user" type="text" placeholder="usuario" class="b-form-element"/><input id="password" type="password" placeholder="contraseña" class="b-form-element"/></div></form><a id="login-button" class="button">Login</a>';
+    '<form id="login-form"> <div><input id="user" type="text" placeholder="usuario" class="b-form-element"/><input id="password" type="password" placeholder="contraseña" class="b-form-element"/></div></form><a id="login-button" class="button">Login</a><div id="error-message"></div>';
   const loginButton = document.getElementById("login-button");
+  
   loginButton.addEventListener("click", tryLogin);
 } else {
   console.log("signed id");
@@ -10,6 +11,8 @@ if (!localStorage.getItem("token")) {
 }
 
 async function tryLogin() {
+  const errorMessage = document.getElementById("error-message");
+  errorMessage.textContent = "";
   const res = await fetch("http://localhost:8000/usuarios/login/", {
     method: "POST",
     headers: {
@@ -21,24 +24,24 @@ async function tryLogin() {
       password: document.getElementById("password").value,
     }),
   });
-console.log(res.status);
+  console.log(res.status);
   const response = await res.json();
-  console.log(res.status,  response.token);
-  if (res.status===200 && response.token){
-  localStorage.setItem("token", response.token);
-  localStorage.setItem("rol", response.rol);
-  localStorage.setItem("id", response.id);
-  localStorage.setItem("name", document.getElementById("user").value);
-  console.log(response);
-  goToMain();}
-  else {
-    const errorMessage = document.createElement("div");
+  console.log(res.status, response.token);
+  if (res.status === 200 && response.token) {
+    localStorage.setItem("token", response.token);
+    localStorage.setItem("rol", response.rol);
+    localStorage.setItem("id", response.id);
+    localStorage.setItem("name", document.getElementById("user").value);
+    console.log(response);
+    goToMain();
+  } else {
+    
     errorMessage.textContent = "Usuario y/o contraseña incorrectos";
-    errorMessage.style.color = "red";
-    mainContainer.appendChild(errorMessage);
+    
+    
   }
 }
-function goToMain(){
+function goToMain() {
   const currentPath = window.location.pathname;
   const newPath = currentPath.replace(/index\.html$/, "pages/main.html");
   window.location.href = newPath;
